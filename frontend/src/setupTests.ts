92ce55ca88e81355302685a6d05de9120b131129
WeatherboySuper <SuperPlays1020@gmail.com>
2025-07-14 10:29:55 -0500

    test: mock global Notification API for testing

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,30 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock global Notification API for testing
+const mockNotificationConstructor = function(this: Notification, title: string, options?: NotificationOptions) {
+  Object.defineProperty(this, 'title', {
+    value: title,
+    writable: false,
+    configurable: true,
+    enumerable: true,
+  });
+  Object.defineProperty(this, 'body', {
+    value: options?.body,
+    writable: false,
+    configurable: true,
+    enumerable: true,
+  });
+  this.close = () => {
+    throw new Error('Method not implemented.');
+  };
+};
+
+mockNotificationConstructor.permission = 'default' as NotificationPermission;
+mockNotificationConstructor.requestPermission = () => Promise.resolve('granted' as NotificationPermission);
+
+Object.defineProperty(global, 'Notification', {
+  value: mockNotificationConstructor,
+  configurable: true,
+});
